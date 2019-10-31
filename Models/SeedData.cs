fefe499d8e9b6843279bb013467494bd289386b6
dporter14 <dporter14@csub.edu>
2019-10-30 19:47:19 -0700

    added data validation and defaults to cabins

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -23,66 +23,50 @@ namespace TRAILES.Models
                     new Cabin
                     {
                         Name = "Everest #101",
-                        AddDate = DateTime.Now,
                         Gender = "Male",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Everest #203",
-                        AddDate = DateTime.Now,
                         Gender = "Male",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Alpine #101",
-                        AddDate = DateTime.Now,
                         Gender = "Male",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Holly #2",
-                        AddDate = DateTime.Now,
                         Gender = "Male",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Alder #104",
-                        AddDate = DateTime.Now,
                         Gender = "Female",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Alder #202",
-                        AddDate = DateTime.Now,
                         Gender = "Female",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Laurel #202",
-                        AddDate = DateTime.Now,
                         Gender = "Female",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     },
                     new Cabin
                     {
                         Name = "Pinyon #101",
-                        AddDate = DateTime.Now,
                         Gender = "Female",
-                        BedCount = 9,
-                        BedsFilled = 0
+                        BedCount = 9
                     }
                 );
                 context.SaveChanges();
