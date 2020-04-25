d3bddd8a2de18e0aee193bf4507086c55d579f55
dporter14 <dporter14@csub.edu>
2020-04-25 13:59:17 -0700

    Cabin sign up works as advertised need to lock down the other pages though.

--- Pages/Cabins/Details.cshtml.cs
+++ Pages/Cabins/Details.cshtml.cs
@@ -7,16 +7,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TRAILES.Data;
 using TRAILES.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace TRAILES.Pages.Cabins
 {
     public class DetailsModel : PageModel
     {
         private readonly TRAILES.Data.AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public DetailsModel(TRAILES.Data.AppDbContext context)
+        public DetailsModel(TRAILES.Data.AppDbContext context,
+                            UserManager<IdentityUser> userManager,
+                            SignInManager<IdentityUser> signInManager)
         {
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public Cabin Cabin { get; set; }
@@ -41,6 +48,42 @@ namespace TRAILES.Pages.Cabins
                 return NotFound();
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var cabin = await _context.Cabins.FindAsync(id);
+            if (user == null || cabin == null)
+            {
+                return NotFound();
+            }
+
+            var studentToReg = await _context.Students.FindAsync(user.Id);
+            if (studentToReg == null 
+                || studentToReg.Registered 
+                || studentToReg.Gender != cabin.Gender
+                || cabin.BedsRegistered == cabin.BedCount)
+            {
+                return Forbid();
+            }
+
+            var oldCabin = await _context.Cabins.FindAsync(studentToReg.CabinID);
+
+            if (await TryUpdateModelAsync<Student>(
+                studentToReg,
+                "student",
+                s => s.CabinID
+            ))
+            {
+                oldCabin.BedsRegistered--;
+                studentToReg.Registered = true;
+                studentToReg.CabinID = id;
+                cabin.BedsRegistered++;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
