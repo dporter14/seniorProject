18f4da4d3a3d9093102c8765ddf4a256374db340
dporter14 <dporter14@csub.edu>
2020-04-26 16:58:55 -0700

    MVP for cabin sign up is officially complete students can register for a cabin, cannot access any other resources. Admin can see all.

--- Pages/Cabins/Index.cshtml.cs
+++ Pages/Cabins/Index.cshtml.cs
@@ -19,11 +19,57 @@ namespace TRAILES.Pages.Cabins
             _context = context;
         }
 
+        public string NameSort {get; set;}
+        public string CountSort {get; set;}
         public IList<Cabin> Cabin { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Cabin = await _context.Cabins.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "" : "";
+            CountSort = sortOrder == "BedsRegistered" ? "reg_desc" : "BedsRegistered";
+
+            var username = User.Identity.Name;
+            if (String.IsNullOrEmpty(username))
+            {
+                RedirectToPage("/Index");
+            }
+            var user = await _context.Students
+                .Where(s => s.UserName == username)
+                .FirstOrDefaultAsync();
+            
+            if (user == null)
+            {
+                RedirectToPage("/Index");
+            }
+
+            IQueryable<Cabin> cabinsIQ = from c in _context.Cabins 
+                                            select c;
+
+            switch(sortOrder)
+            {
+                case "reg_desc":
+                    cabinsIQ = cabinsIQ.OrderByDescending(c => c.BedsRegistered);
+                    break;
+                case "BedsRegistered":
+                    cabinsIQ = cabinsIQ.OrderBy(c => c.BedsRegistered);
+                    break;
+                default:
+                    cabinsIQ = cabinsIQ.OrderBy(c => c.Name);
+                    break;
+            }
+            if (User.Identity.Name != "admin@bchstest.com")
+            {
+                Cabin = await cabinsIQ
+                    .Where(c => c.Gender == user.Gender)
+                    .AsNoTracking()
+                    .ToListAsync();
+            }
+            else
+            {
+                Cabin = await cabinsIQ
+                    .AsNoTracking()
+                    .ToListAsync();
+            }
         }
     }
 }
